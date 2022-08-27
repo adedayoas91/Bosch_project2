f210d30673969a2319d9cdcfa752d3aca89621fa
Sadiq <adedayoas91@gmail.com>
2022-08-27 09:35:24 +0000

    Required changes made

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -46,5 +46,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskbright.azurewebsites.net:$PORT/predict
+     -X POST https://flaskbri.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
